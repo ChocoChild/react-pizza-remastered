e4d95ca821467283369da305c9cc91d5d6f11927
ChocoChild <nijatgasimzada@gmail.com>
2023-05-21 10:07:03 +0400

    Подключение элемента загрузки страницы

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import axios from 'axios';
 import { Header, Categories, Sort, PizzaBlock } from './components/allComponents.js';
 import './scss/app.scss';
+import LoadingSkeleton from './components/PizzaBlock/LoadingSkeleton.jsx';
 
 function App() {
   const [pizzasStore, setPizzasStore] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
   React.useEffect(() => {
     axios.get('https://646910e803bb12ac20855e11.mockapi.io/pizzaStore')
     .then(({ data }) => {
-      console.log(data)
       setPizzasStore(data[0].pizzas)
+      setIsLoading(false);
     })
   },[])
   return (
@@ -23,9 +25,10 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzasStore.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading 
+            ? [...new Array(6)].map((_, index) => <LoadingSkeleton key={index} />)
+            : pizzasStore.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+          }
           </div>
         </div>
       </div>
